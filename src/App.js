cc7213e14f90fe62b086656e6d22c200a6c9df22
Nguyễn Hiếu <59680580+hieuls369@users.noreply.github.com>
2021-05-13 14:12:06 +0700

    useStateHook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import ColorBox from './component/ColorBox';
+import TodoForm from './component/TodoForm';
+import TodoList from './component/TodoList';
 
 function App() {
+
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: 'I love Easy Frontend!' },
+    { id: 2, title: 'We love Easy Frontend' },
+    { id: 3, title: 'They love Easy Frontend' }
+  ])
+
+  function handleTodoClick(todo) {
+    console.log(todo);
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList)
+  }
+
+  function handleTodoFormSubmit(formValue) {
+    console.log('Form Submit:', formValue);
+    const newTodo = {
+      id: todoList.length + 1,
+      ...formValue
+    }
+
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
+
+    setTodoList(newTodoList)
+  }
+
   return (
     <div className="App">
-      <h1>Welcome to react hook</h1>
+      <h1>Welcome to react hook-ColorBox-TodoList</h1>
+      <ColorBox />
+      <TodoForm onSubmit={handleTodoFormSubmit} />
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
+
     </div>
   );
 }
---
+++ src/component/ColorBox/index.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './colorBox.scss';
+
+ColorBox.propTypes = {
+
+};
+
+function getRandomColor() {
+    const COLOR_LIST = ['deeppink', 'green', 'yellow', 'black', 'blue'];
+    const randomIndex = Math.trunc(Math.random() * 5);
+    return COLOR_LIST[randomIndex];
+}
+
+function ColorBox() {
+
+    const [color, setColor] = useState(() => {
+        const initialColor = localStorage.getItem('box_color') || 'deeppink';
+        return initialColor;
+    });
+
+
+    function handleBoxClick() {
+        const newColor = getRandomColor();
+        setColor(newColor);
+        localStorage.setItem('box_color', newColor)
+    }
+
+    return (
+        <div
+            className="color-box"
+            style={{ backgroundColor: color }}
+            onClick={handleBoxClick}
+        >
+
+        </div>
+    );
+}
+
+export default ColorBox;
